b1274dc104fbd02f5a645c3ce13ebc7043e3811e
maxime-pico <pico.maxime@gmail.com>
2022-04-08 21:27:58 +0200

    try another url

--- src/App.tsx
+++ src/App.tsx
@@ -179,7 +179,7 @@ class App extends Component<AppProps, AppState> {
 
   connectSocket = async () => {
     const socket = await socketService
-      .connect("https://git.heroku.com/shielded-ocean-17543.git:9000")
+      .connect("http://kombass-server.herokuapp.com:9000")
       .catch((e: string) => console.log("Error on connect: ", e));
   };
 
